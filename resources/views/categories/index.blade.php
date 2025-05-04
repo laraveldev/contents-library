5c1d4dc193065db58119ab0e1389cba1fccbd578
elnurbek <elnurbek.pubgmobile@gmail.com>
2025-05-04 18:08:54 +0500

    change a href route

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,7 +2,7 @@
         <x-app-layout>
             <x-slot name="header">
                 <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    <a href="{{ url('/authors/create') }}"
+                    <a href="{{ url('/categories/create') }}"
                        class="inline-block mt-2 px-4 py-2 bg-green-600 hover:bg-green-700 text-white text-sm font-medium rounded">
                         Create new category
                     </a>
