3506277bc5f61b1c8cbaae37e4865725c2f02472
elnurbek <elnurbek.pubgmobile@gmail.com>
2025-05-07 17:30:20 +0500

    for admin

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Admin Profile') }}
+            @role('admin'){{ __('Admin Profile') }} @endrole
+            
         </h2>
         
     </x-slot>
