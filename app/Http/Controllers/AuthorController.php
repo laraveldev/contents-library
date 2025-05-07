45392e7130f7617926a3805ef0fc08c0bce9aa79
elnurbek <elnurbek.pubgmobile@gmail.com>
2025-05-07 15:07:58 +0500

    authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -55,32 +55,3 @@ class AuthorController extends Controller
         return redirect()->route('authors')->with('success', 'author ochirildi!');
     }
 }
-
-public function makeAdmin(User $user)
-    {
-        // Faol foydalanuvchi super admin (id = 1) bo‘lishi kerak
-        if (auth()->user()->id !== 1) {
-            return redirect()->back()->with('error', 'Faqat Super Admin admin tayinlay oladi.');
-        }
-
-        if (!$user->hasRole('admin')) {
-            $user->assignRole('admin');
-        }
-
-        return redirect()->back()->with('success', "$user->name admin sifatida tayinlandi.");
-    }
-
-    // Foydalanuvchidan adminlikni olib tashlash
-    public function removeAdmin(User $user)
-    {
-        // Faol foydalanuvchi super admin (id = 1) bo‘lishi kerak
-        if (auth()->user()->id !== 1) {
-            return redirect()->back()->with('error', 'Faqat Super Admin adminlik huquqini olib tashlay oladi.');
-        }
-
-        if ($user->hasRole('admin')) {
-            $user->removeRole('admin');
-        }
-
-        return redirect()->back()->with('success', "$user->name endi admin emas.");
-    }
