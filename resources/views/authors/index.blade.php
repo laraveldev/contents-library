7b05721f12adfd89995fbce73a01318a97c6e5dc
elnurbek <elnurbek.pubgmobile@gmail.com>
2025-05-12 16:43:32 +0500

    name +

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
+        Authors
         @role('admin')
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             <a href="{{ url('/authors/create') }}"
@@ -8,6 +9,7 @@
             </a>
         </h2>
         @endrole
+        
     </x-slot>
 
     <div class="py-12">
